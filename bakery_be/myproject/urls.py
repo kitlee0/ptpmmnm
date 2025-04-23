b619cbe8d04d2d559ab21ba1354c822a5047cdf9
kitlee0 <1150080022@sv.hcmunre.edu.vn>
2025-04-23 16:06:41 +0700

    my project update

---
+++ bakery_be/myproject/urls.py
@@ -0,0 +1,10 @@
+# myproject/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('cakeAPI.urls')),
+    path('', include('cakeAPI.urls')),
+]
